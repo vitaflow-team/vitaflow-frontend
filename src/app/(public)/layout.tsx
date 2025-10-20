0e107019890289cdbf3b04fc965f3529642b5be5
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-10-20 18:38:50 -0300

    feat: add custom 404 error page

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,5 @@
 import { Logo } from '@/_components/layout/logo';
-import { NavLink } from '@/_components/layout/navLink.tsx';
+import { NavLink } from '@/_components/layout/navLink';
 import { PublicFooter } from '@/_components/layout/publicFooter';
 import { Great_Vibes } from 'next/font/google';
 
