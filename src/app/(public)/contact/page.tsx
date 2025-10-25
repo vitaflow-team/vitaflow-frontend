ceb9a564a188283a757b4367c08fa186c36a034c
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-10-25 08:19:00 -0300

    feat(public): add contact page
    Add contact page under public layout with heading and descriptive text.

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -1,3 +1,14 @@
 export default function Contact() {
-  return <h1>Contato</h1>;
+  return (
+    <div className="flex flex-col w-full p-8">
+      <h1>Entre em contato com o VitaFlow</h1>
+      <span>
+        Fale com nossa equipe e descubra como o VitaFlow pode impulsionar seus
+        resultados.
+      </span>
+      <span>
+        Juntos, construímos uma jornada de saúde mais conectada e eficiente.
+      </span>
+    </div>
+  );
 }
