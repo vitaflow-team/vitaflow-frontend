27120dd668e2c55e2426035e542c0a49788a7355
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-11-03 22:18:06 -0300

    fix(layout): adjust padding top default layout

--- src/_components/layout/defaultLayout.tsx
+++ src/_components/layout/defaultLayout.tsx
@@ -6,7 +6,7 @@ export default function DefaultLayout({
   return (
     <main className="flex flex-col w-full h-full">
       <div className="flex flex-row w-full items-center justify-center gap-20">
-        <div className="flex flex-col w-full gap-2 md:gap-4 pt-2 md:pt-4">
+        <div className="flex flex-col w-full gap-2 md:gap-4 pt-2">
           {children}
         </div>
       </div>
