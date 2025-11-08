9e937ab5816d5c18cad97cc98c8cf79f4e3787a7
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-11-08 09:47:46 -0300

    feat: add schema and form for schedule creation

---
+++ src/_schema/workout.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const workoutSchema = z.object({
+  id: z.string().optional(),
+  description: z
+    .string()
+    .min(2, 'A descrição do treino é obrigatótia')
+    .max(200, 'Descrição muito longa'),
+});
+
+export type workoutFormData = z.infer<typeof workoutSchema>;
