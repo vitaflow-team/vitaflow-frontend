fef7c5b28f3750c70e1ae381828b897c54736587
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-11-11 21:18:04 -0300

    style(components): adjust styling to align with design system

--- src/_components/ui/title.tsx
+++ src/_components/ui/title.tsx
@@ -3,6 +3,7 @@ import { HTMLAttributes } from 'react';
 
 interface TitleProps extends HTMLAttributes<HTMLDivElement> {
   size?: 'h1' | 'h2' | 'h3';
+  styled?: 'default' | 'form';
   label: string;
 }
 
@@ -10,6 +11,7 @@ export function Title({
   size = 'h1',
   className,
   label,
+  styled = 'default',
   children,
   ...props
 }: TitleProps) {
@@ -19,18 +21,23 @@ export function Title({
       h2: 'text-base pt-1',
       h3: 'text-xs',
     },
+    styled: {
+      default: '',
+      form: 'justify-between border-b border-primary text-left py-1',
+    },
   };
 
   return (
     <div
       {...props}
       className={cn(
-        'w-full text-center font-semibold',
+        'flex flex-col md:flex-row items-center w-full text-center font-semibold mb-2',
         buttonClass.size[size],
+        buttonClass.styled[styled],
         className
       )}
     >
-      <span className="flex w-full">{label}</span>
+      <span className="w-full">{label}</span>
       {children}
     </div>
   );
