2b0ec7e198d1da9e47972462434b9af05a6374e0
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-11-12 21:15:21 -0300

    feat(types): create daysofweek enum

---
+++ src/_enumerator/daysOfWeek.ts
@@ -0,0 +1,9 @@
+export const DAYS_OF_WEEK = [
+  { id: 'Sun', label: 'Domingo' },
+  { id: 'Mon', label: 'Segunda' },
+  { id: 'Tue', label: 'Terça' },
+  { id: 'Wed', label: 'Quarta' },
+  { id: 'Thu', label: 'Quinta' },
+  { id: 'Fri', label: 'Sexta' },
+  { id: 'Sat', label: 'Sábado' },
+] as const;
