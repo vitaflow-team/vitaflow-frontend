27568b702a3ea6002e285e98fee2b5cda980c067
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-12-16 20:59:34 -0300

    feat: implement rate limiting for authentication routes and add middleware for auth checks

---
+++ src/_lib/ratelimit.ts
@@ -0,0 +1,9 @@
+import { Ratelimit } from '@upstash/ratelimit';
+import { kv } from '@vercel/kv';
+
+export const ratelimit = new Ratelimit({
+  redis: kv,
+  limiter: Ratelimit.slidingWindow(5, '10 s'),
+  analytics: true,
+  prefix: '@upstash/ratelimit',
+});
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,29 @@ import { auth } from '@/auth';
 import { NextResponse } from 'next/server';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default auth((req: any) => {
+export default auth(async (req: any) => {
   const path = req.nextUrl.pathname;
   const isLogged = !!req.auth;
+
+  if (
+    path === APP_ROUTES.SIGN_IN ||
+    path === APP_ROUTES.SIGN_UP ||
+    path.startsWith('/api/auth')
+  ) {
+    if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
+      try {
+        const { ratelimit } = await import('@/_lib/ratelimit');
+        const ip = req.headers.get('x-forwarded-for') ?? '127.0.0.1';
+        const { success } = await ratelimit.limit(ip);
+
+        if (!success) {
+          return new NextResponse('Too Many Requests', { status: 429 });
+        }
+      } catch (error) {
+        console.warn('Rate Limit Error:', error);
+      }
+    }
+  }
 
   if (path.startsWith(APP_ROUTES.PRIVATE.DASHBOARD) && !isLogged) {
     return NextResponse.rewrite(new URL(APP_ROUTES.HOME, req.url));
